532142ca3e8e24503b46577832ad4567bb6cf080
lu-yi-hsun <yihsun1992@gmail.com>
2018-01-06 20:39:00 +0800

    Deployed 44db4d5 with MkDocs version: 0.17.2

---
+++ my_lib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yihsun1992',
+      version='0.2',
+      description='my-lib on tensorflow',
+      url='',
+      author='lu-yi-hsun',
+      author_email='yihsun1992@gmail.com',
+      license='MIT',
+      packages=['yihsun1992'],
+      zip_safe=False)

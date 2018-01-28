057de47946aad6fcf412690ceb8268fac4458aff
lu-yi-hsun <yihsun1992@gmail.com>
2018-01-28 21:07:56 +0800

    Deployed 151d03111 with MkDocs version: 0.17.2

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,23 +1,8 @@
 window.MathJax = {
   jax: ["input/TeX","output/SVG"],
-  tex2jax: {
-    inlineMath: [ ["\\(","\\)"] ],
-    displayMath: [ ["\\[","\\]"] ]
-  },
-  TeX: {
-    TagSide: "right",
-    TagIndent: ".8em",
-    MultLineWidth: "85%",
-    equationNumbers: {
-      autoNumber: "AMS",
-    },
-    unicode: {
-      fonts: "STIXGeneral,'Arial Unicode MS'"
-    }
-  },
-  displayAlign: "left",
-  showProcessingMessages: false,
-  messageStyle: "none"
+  
+ 
+  
 };
 
 

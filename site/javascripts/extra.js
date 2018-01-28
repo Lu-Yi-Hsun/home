8d594b8dfdce855c05e7bb8cf8061c98d7f13204
lu-yi-hsun <yihsun1992@gmail.com>
2018-01-28 21:38:36 +0800

    small file

--- site/javascripts/extra.js
+++ site/javascripts/extra.js
@@ -1,5 +1,9 @@
 window.MathJax = {
-  jax: ["input/TeX","output/SVG"],
+  jax: ["input/TeX","input/MathML","input/AsciiMath","output/SVG"],
+  extensions: ["tex2jax.js","mml2jax.js","asciimath2jax.js","MathMenu.js","MathZoom.js","AssistiveMML.js", "a11y/accessibility-menu.js"],
+  TeX: {
+    extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
+  },
   tex2jax: {
     inlineMath: [ ["\\(","\\)"] ],
     displayMath: [ ["\\[","\\]"] ]
